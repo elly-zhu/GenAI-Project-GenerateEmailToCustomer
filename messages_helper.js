7f3400109b8385f086f82a875473d090cd822d27
elly-zhu <yzhu.elly@gmail.com>
2023-10-10 10:18:27 -0700

    Update messages_helper.js

--- messages_helper.js
+++ messages_helper.js
@@ -35,29 +35,20 @@ export function form_user_content(userInput) {
 export function form_assistant_content_about_product(products, userInput) {
   userInput = userInput.replace("?", "");
   const product_name_list = get_product_names(products);
-  const categories_list = get_product_categories(products);
   let relevant_products = [];
-  for (let word of userInput.split(" ")) {
-    for (let product_name of product_name_list) {
-      if (product_name.includes(word)) {
-        relevant_products.push(get_product_by_name(products, word));
-      }
-    }
-    if (categories_list.includes(word)) {
-      for (let category of categories_list) {
-        if (category.includes(word)) {
-          relevant_products.concat(get_products_by_category(products, word));
-        }
-      }
+  for (let product_name of product_name_list) {
+    if (userInput.includes(product_name)) {
+      relevant_products.push(products[product_name]);
     }
   }
+
   // remove duplicates in the list
   relevant_products = [...new Set(relevant_products)];
   let res = `the relavent informations: ` + JSON.stringify(relevant_products);
-  
+
   // NOTE: THIS CUTS THE RELEVANT DATA, IT WILL AFFECT THE ACCURACY, THE REASON FOR THIS IS TO LIMIT THE TOKEN USED
-  // A BETTER WAY TO DO THIS IS TO INCREASE THE MAX TOKEN OR IMPROVE THE DATA MATCHING 
-  res = res.substring(0, 1000);
+  // YOU COULD INCREASE THIS LIMIT
+  res = res.substring(0, 1200);
   return res;
 }
 
